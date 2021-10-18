912816bfec85d9f6a64395f77e3aedc1ce9e6d1f
dazzlemon <daanill13470@gmail.com>
2021-10-18 23:39:09 +0300

    pdf plot almost done

--- 2/src/main.py
+++ 2/src/main.py
@@ -16,9 +16,13 @@ def group(data, bin_edges):
     boundaries_idx = np_map(lambda i: np.argmax(dlist >= i), bin_edges[:-1])[1:]
     return np.array_split(dlist, boundaries_idx)
 
-def pdf():
-    """Probability density function"""
-    freq_density = freq / width
+def pdf_plot(bin_edges, freq):
+    """Plots Probability density function"""
+    bin_means = np_map(lambda x: (x[0] + x[1]) / 2, pairwise(bin_edges))
+    plt.bar(
+        bin_means, stats.rv_histogram((freq, bin_edges)).pdf(bin_means),
+        label='Probability Density Function'
+    )
 
 def freq_poly(bin_edges, freq):
     """Plots Frequency Polygon"""
@@ -36,14 +40,13 @@ def main():
     #     180, 155, 149, 176, 181, 146, 105, 191, 163, 116, 113, 182, 149, 195, 147,
     #     146, 113, 185, 155, 149, 180, 131, 184, 198, 119, 122, 160, 153, 109, 158,
     # ]
-
     data = [
         6.0,  9.1, 8.7, 6.7, 5.4, 10.9, 9.9, 9.4, 9.9, 9.4, 9.2, 9.6,
         8.1,  7.8, 9.5, 8.2, 9.7,  8.1, 8.5, 9.5, 7.9, 8.9, 8.9, 9.7,
         5.7, 11.4, 9.7, 9.2, 9.8, 10.6,
     ]
-
     amount_bins = 6#5
+    
     width = len(data) / amount_bins
     range_ = (min(data), max(data))
 
@@ -61,7 +64,8 @@ def main():
     print(tabulate(dict_, headers='keys', tablefmt='psql'))
 
     # freq_poly(bin_edges, hist)
-    cumfreq_plot(bin_edges, data)
+    # cumfreq_plot(bin_edges, data)
+    pdf_plot(bin_edges, hist / width)
 
     plt.legend(loc='best')
     plt.show()
